d21904e7a22b0c8b569bdd4842f46d5a370e6246
Jason Lu <jasonl.biz@gmail.com>
2013-06-03 00:11:03 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scholarship::Application.config.secret_token = '6e23d0ef467ab1aed780a1dde03a43f2180ca3a275b22ced803f391747fd77fb0615028ad4eef87d54fef5ff7b6511910f771b26df6040cd8fc87c0339eb213d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Scholarship::Application.config.session_store :cookie_store, key: '_scholarship_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Scholarship::Application.config.session_store :active_record_store
