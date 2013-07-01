6aef667e34376ddda3723da1d07a7f609070a9fe
Jason Lu <jason@eatgo.com>
2013-07-01 15:21:56 +0800

    wip

---
+++ config/deploy.rb
@@ -0,0 +1,44 @@
+require 'capistrano/ext/multistage'
+
+set :user, "deploy"  # The server's user for deploys
+set :application, "Scholarship"
+set :repository,  "git@github.com:jasonlu/scholarship.git"
+set :keep_release, 5
+set :use_sudo, false
+
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+set :scm_passphrases, ""
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :stages, ["staging", "production"]
+set :default_stage, "staging"
+
+namespace :bundle do
+
+  desc "run bundle install and ensure all gem requirements are met"
+  task :install do
+    run "cd #{current_path} && bundle install  --without=test --no-update-sources"
+  end
+
+end
+before "deploy:restart", "bundle:install"
+
+
+
+
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
---
+++ config/deploy/production.rb
@@ -0,0 +1,4 @@
+#server "10.10.1.100", :app, :web, :db, :primary => true
+server "54.250.144.204", :web
+#server "10.10.1.106", :web
+set :deploy_to, "/srv/www/onlynet.biz"
